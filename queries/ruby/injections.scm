c0453bd596930bfea7b1008b4ab7a19ff93abf2d
derekstride <derek.stride@shopify.com>
2024-11-05 16:06:07 -0500

    feat: add injection for ruby debugger command strings

--- queries/ruby/injections.scm
+++ queries/ruby/injections.scm
@@ -9,3 +9,16 @@
 (regex
   (string_content) @injection.content
   (#set! injection.language "regex"))
+
+(
+  (call
+    receiver: (identifier) @receiver
+    method: (identifier) @method
+    arguments: (argument_list
+      (pair
+        key: (hash_key_symbol)
+        value: (string (string_content) @injection.content))))
+  (#eq? @receiver "binding")
+  (#eq? @method "b")
+  (#set! injection.language "ruby")
+)
